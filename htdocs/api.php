d1c83aab4bf0c44c07a4aa4b8685accbdc505b88
Andrew Milsted <a.j.milsted@soton.ac.uk>
2014-11-21 10:11:21 +0000

    start of api

---
+++ htdocs/api.php
@@ -0,0 +1,22 @@
+<?php
+
+##############################
+# notes
+##############################
+
+# category
+
+# date - default gives site/field + site is uni?
+# site - gives date/field 
+# field
+
+# list dates
+# list sites (inc relevant dates?)
+# list fields (inc relevant dates?)
+
+# /date
+# /site
+# /field
+# /date/2014-07-01
+# /site/soton.ac.uk/
+
